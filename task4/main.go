5551e1ac95aa003e4525cc144f1d1fc10b21d705
Oleg Petrov <42185589+petrolax@users.noreply.github.com>
2021-08-02 17:03:02 +0300

    Add interrupt handler
    Добавлен обработчик прерываний, для нормального завершения горутин.

--- task4/main.go
+++ task4/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"os"
+	"os/signal"
 	"sync"
 )
 
-func worker(id int, c <-chan string, wg *sync.WaitGroup) {
+var (
+	IsClosed = false
+	once sync.Once
+)
+
+func worker(ctx context.Context, id int64, c chan string, wg *sync.WaitGroup) {
 	for {
 		select {
 		case val, ok := <-c:
@@ -18,39 +25,61 @@ func worker(id int, c <-chan string, wg *sync.WaitGroup) {
 			}
 			fmt.Printf("Worker%d: %s\n", id, val)
 			fmt.Print("Enter data: ")
+		case <-ctx.Done():
+			once.Do(func() {
+				close(c)
+				IsClosed = true
+			})
+			
+			fmt.Println("Worker", id, "was dead")
+			wg.Done()
+			return
 		}
 	}
 }
 
-func startWorkers(amount int, c <-chan string, wg *sync.WaitGroup) {
-	for i := 0; i < amount; i++ {
+func startWorkers(ctx context.Context, amount int64, c chan string, wg *sync.WaitGroup) {
+	for i := int64(0); i < amount; i++ {
 		wg.Add(1)
-		go worker(i, c, wg)
+		go worker(ctx, i, c, wg)
 	}
 }
 
 func main() {
-	data := make(chan string)
-	var wg sync.WaitGroup
-	amountOfWorkers := 0
+	var amountOfWorkers int64
 	fmt.Print("Enter amout of workers: ")
 	fmt.Scanf("%d\n", &amountOfWorkers)
 	if amountOfWorkers == 0 {
 		panic("Workers can't be 0")
 	}
-
-	startWorkers(amountOfWorkers, data, &wg)
+	
+	ctx, cancel := context.WithCancel(context.Background())
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+	data := make(chan string)
+	var wg sync.WaitGroup
+	
+	startWorkers(ctx, amountOfWorkers, data, &wg)
+	go func() {
+		for {
+			select {
+			case <-signals:
+				cancel()
+				return
+			}
+		}
+	}()
 
 	input := bufio.NewScanner(os.Stdin)
 	fmt.Print("Enter data: ")
 	for input.Scan() {
 		str := input.Text()
+		if IsClosed { break}
 		if str == "stop" || str == "STOP" {
 			close(data)
 			break
 		}
 		data <- str
-		// fmt.Print("Enter data: ")
 	}
 	wg.Wait()
 }
